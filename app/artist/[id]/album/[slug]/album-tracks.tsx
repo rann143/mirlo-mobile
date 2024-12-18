c6a405e19dfb8788d2c57fa73d92665474dde8fa
Daniel <roderm99@gmail.com>
2024-12-18 15:05:49 -0500

    drafting playSound functionality for first track in album (for now)

--- app/artist/[id]/album/[slug]/album-tracks.tsx
+++ app/artist/[id]/album/[slug]/album-tracks.tsx
@@ -1,15 +1,37 @@
-import { Text, View, SafeAreaView, StyleSheet, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { API_ROOT } from "@/constants/api-root";
 import { useState, useEffect } from "react";
 import { VscPlay } from "react-icons/vsc";
+import { Audio } from "expo-av";
 
 type TrackProps = {
   title: string;
+  audio: {
+    url: string;
+  };
 };
 
-const TrackItem = ({ title }: TrackProps) => (
+// Not Yet Functional
+const PlayButton = () => {
+  return (
+    <TouchableOpacity style={styles.appButtonContainer}>
+      <Text style={styles.appButtonText}>Play</Text>
+    </TouchableOpacity>
+  );
+};
+
+const TrackItem = ({ title, audio }: TrackProps) => (
   <View style={styles.listItem}>
+    <PlayButton />
     <Text style={{ color: "white", fontSize: 20 }}>{title}</Text>
   </View>
 );
@@ -18,24 +40,63 @@ export default function AlbumTracks() {
   const { id, slug } = useLocalSearchParams();
   const [tracks, setTracks] = useState<TrackProps[]>([]);
 
+  // ************************
+  // Current Goal is just to get first song in tracks to play. Then focus on being able to play all displayed songs
+  const [currentSound, setCurrentSound] = useState<Audio.Sound | null>(null);
+
+  async function playSound() {
+    try {
+      console.log("Loading Sound");
+      // Unload existing sound if any
+      if (currentSound) {
+        await currentSound.unloadAsync();
+      }
+
+      const soundUri = API_ROOT + tracks[0].audio.url;
+      // console.log(soundUri);
+
+      const { sound: playBackObject } = await Audio.Sound.createAsync(
+        { uri: soundUri },
+        { shouldPlay: true }
+      );
+
+      setCurrentSound(playBackObject);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  // ************************
+
   useEffect(() => {
     const callback = async () => {
       const fetchedAlbum = await fetch(
-        `${API_ROOT}v1/trackGroups/${slug}/?artistId=${id}`
+        `${API_ROOT}/v1/trackGroups/${slug}/?artistId=${id}`
       ).then((response) => response.json());
       setTracks(fetchedAlbum.result.tracks);
     };
     callback();
-  }, []);
+
+    return () => {
+      if (currentSound) {
+        currentSound.unloadAsync();
+      }
+    };
+  }, [currentSound]);
+
+  // console.log(tracks);
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
+        <Button title="Play First Track (Testing)" onPress={playSound} />
         <FlatList
           style={{ width: "100%" }}
           contentContainerStyle={styles.listContainer}
           data={tracks}
-          renderItem={({ item }) => <TrackItem title={item.title}></TrackItem>}
+          renderItem={({ item }) => (
+            <TrackItem title={item.title} audio={item.audio}></TrackItem>
+          )}
         ></FlatList>
       </View>
     </SafeAreaView>
@@ -66,5 +127,19 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 4,
     backgroundColor: "#f0f0f0", // placeholder color while loading
+  },
+  appButtonContainer: {
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    marginRight: 10,
+  },
+  appButtonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase",
   },
 });
