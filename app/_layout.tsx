ffa55812da7df74b0e3733b6b1bf7924f9f44edc
Daniel <roderm99@gmail.com>
2024-12-19 18:38:27 -0500

    wrap app in player context provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,25 @@
 import { Stack, usePathname } from "expo-router";
+import { PlayerContextProvider } from "@/state/PlayerContext";
 
 export default function RootLayout() {
   const pathname = usePathname();
 
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          title: "Mirlo",
-        }}
-      />
-      <Stack.Screen
-        name="artist/[id]/album/[slug]"
-        options={{
-          title: "Album",
-        }}
-      />
-    </Stack>
+    <PlayerContextProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Mirlo",
+          }}
+        />
+        <Stack.Screen
+          name="artist/[id]/album/[slug]"
+          options={{
+            title: "Album",
+          }}
+        />
+      </Stack>
+    </PlayerContextProvider>
   );
 }
