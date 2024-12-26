37457ff8944cd68c74dbed828000b8c32e6675f5
Daniel <roderm99@gmail.com>
2024-12-26 13:02:58 -0500

    add profile icon to header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack, usePathname } from "expo-router";
 import { PlayerContextProvider } from "@/state/PlayerContext";
+import Feather from "@expo/vector-icons/Feather";
+import { Link } from "expo-router";
+
+function ProfileLink() {
+  const icon = <Feather name="user" size={24} color="black" />;
+  return <Link href="/">{icon}</Link>;
+}
 
 export default function RootLayout() {
   const pathname = usePathname();
@@ -11,12 +18,14 @@ export default function RootLayout() {
           name="index"
           options={{
             title: "Mirlo",
+            headerRight: () => <ProfileLink />,
           }}
         />
         <Stack.Screen
           name="artist/[id]/album/[slug]"
           options={{
             title: "Album",
+            headerRight: () => <ProfileLink />,
           }}
         />
       </Stack>
