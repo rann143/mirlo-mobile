0c19b89e03cf6f3c43a688f0fddda1ba700a73dc
Daniel <roderm99@gmail.com>
2024-12-27 17:38:07 -0500

    create login page with basic login form; no functionality added yet

---
+++ app/login.tsx
@@ -0,0 +1,17 @@
+import LoginForm from "@/components/LoginForm";
+import { Text, View, SafeAreaView, StyleSheet } from "react-native";
+
+export default function Login() {
+  return (
+    <SafeAreaView style={styles.form}>
+      <LoginForm />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  form: {
+    backgroundColor: "#BE3455",
+    flex: 1,
+  },
+});
