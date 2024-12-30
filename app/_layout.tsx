231b3822c9ce87e2d216913e3bc5adbe7a483b00
Daniel <roderm99@gmail.com>
2024-12-29 19:18:22 -0500

    add QueryCLientWrapper and DevToolsBubble to root layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,41 @@
 import { Stack, usePathname } from "expo-router";
 import { PlayerContextProvider } from "@/state/PlayerContext";
-import Feather from "@expo/vector-icons/Feather";
-import { Link } from "expo-router";
 import ProfileLink from "@/components/ProfileLink";
+import { QueryClientWrapper } from "@/queries/QueryClientWrapper";
+import { DevToolsBubble } from "react-native-react-query-devtools";
+import * as Clipboard from "expo-clipboard";
 
 export default function RootLayout() {
   const pathname = usePathname();
 
+  const onCopy = async (text: string) => {
+    try {
+      await Clipboard.setStringAsync(text);
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
   return (
-    <PlayerContextProvider>
-      <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{ headerShown: false, title: "Recent Releases" }}
-        />
-        <Stack.Screen
-          name="artist/[id]/album/[slug]"
-          options={{
-            title: "Album",
-            headerRight: () => <ProfileLink />,
-          }}
-        />
-        <Stack.Screen name="login" options={{ title: "Login" }} />
-      </Stack>
-    </PlayerContextProvider>
+    <QueryClientWrapper>
+      <PlayerContextProvider>
+        <Stack>
+          <Stack.Screen
+            name="(tabs)"
+            options={{ headerShown: false, title: "Recent Releases" }}
+          />
+          <Stack.Screen
+            name="artist/[id]/album/[slug]"
+            options={{
+              title: "Album",
+              headerRight: () => <ProfileLink />,
+            }}
+          />
+          <Stack.Screen name="login" options={{ title: "Login" }} />
+        </Stack>
+      </PlayerContextProvider>
+      <DevToolsBubble onCopy={onCopy} />
+    </QueryClientWrapper>
   );
 }
