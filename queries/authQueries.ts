2a22164094cb5edb4532e50364957d1fc8fd0784
Daniel <roderm99@gmail.com>
2024-12-29 19:40:49 -0500

    add login/logout functions and mutations

--- queries/authQueries.ts
+++ queries/authQueries.ts
@@ -1,3 +1,9 @@
+import {
+  QueryFunction,
+  queryOptions,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
 import * as api from "./fetch/fetchWrapper";
 
 type LoginBody = {
@@ -7,4 +13,32 @@ type LoginBody = {
 
 async function login(body: LoginBody) {
   await api.post("/auth/login", body);
+}
+
+export function useLoginMutation() {
+  const client = useQueryClient();
+  return useMutation({
+    mutationFn: login,
+    async onSuccess() {
+      await client.invalidateQueries({
+        predicate: (query) => query.queryKey.includes("auth"),
+      });
+    },
+  });
+}
+
+async function logout() {
+  await api.get("/auth/logout", {});
+}
+
+export function useLogoutMutation() {
+  const client = useQueryClient();
+  return useMutation({
+    mutationFn: logout,
+    async onSuccess() {
+      await client.invalidateQueries({
+        predicate: (query) => query.queryKey.includes("auth"),
+      });
+    },
+  });
 }
