bab9b23488dfabdc2958c8d976e2bcc196861171
Daniel <roderm99@gmail.com>
2024-12-29 21:39:35 -0500

    add refresh fn/mutation and fetchProfile fn & queryAuthProfile query

--- queries/authQueries.ts
+++ queries/authQueries.ts
@@ -5,6 +5,7 @@ import {
   useQueryClient,
 } from "@tanstack/react-query";
 import * as api from "./fetch/fetchWrapper";
+import { MirloFetchError } from "./fetch/MirloFetchError";
 
 type LoginBody = {
   email: string;
@@ -40,5 +41,41 @@ export function useLogoutMutation() {
         predicate: (query) => query.queryKey.includes("auth"),
       });
     },
+  });
+}
+
+export async function authRefresh() {
+  await api.post("/auth/refresh", {});
+}
+
+export function useAuthRefreshMutation() {
+  const client = useQueryClient();
+  const { mutate } = useMutation({
+    mutationFn: authRefresh,
+    async onSuccess() {
+      await client.invalidateQueries({
+        predicate: (query) => query.queryKey.includes("auth"),
+      });
+    },
+  });
+}
+
+const fetchProfile: QueryFunction<
+  LoggedInUser | null,
+  ["fetchProfile", ...string[]]
+> = ({ signal }) => {
+  return api
+    .get<{ result: LoggedInUser }>(`/auth/profile`, { signal })
+    .then((r) => r.result)
+    .catch((e) => {
+      if (e instanceof MirloFetchError && e.status === 401) return null;
+      else throw e;
+    });
+};
+
+export function queryAuthProfile() {
+  return queryOptions({
+    queryKey: ["fetchProfile", "auth"],
+    queryFn: fetchProfile,
   });
 }
