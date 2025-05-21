db3454c70f165af48dcae922fe837d8c1f7a0a1a
Daniel <dan.roder17@gmail.com>
2025-05-21 17:33:19 -0400

    Include search-by-tag in search screen

---
+++ components/TagPill.tsx
@@ -0,0 +1,31 @@
+import { Link, useRouter } from "expo-router";
+
+type TagPillProps = {
+  key: number;
+  tagName: string;
+};
+
+export default function TagPill({ tagName }: TagPillProps) {
+  const router = useRouter();
+  return (
+    <Link
+      href={{
+        pathname: "/tags/[tag]",
+        params: { tag: tagName },
+      }}
+      style={{
+        backgroundColor: "#f0f0f0",
+        borderWidth: 1,
+        borderColor: "#e3e1e1",
+        padding: 10,
+        paddingHorizontal: 15,
+        borderRadius: 18,
+        marginRight: 10,
+        marginVertical: 5,
+      }}
+      onPress={() => router.back()}
+    >
+      {tagName}
+    </Link>
+  );
+}
