d7ccf07521c77a599e8bdc6076f755bb4dec3e1d
Daniel <dan.roder17@gmail.com>
2025-07-28 20:47:47 -0400

    create inital util functions

---
+++ scripts/trackPlayUtils.tsx
@@ -0,0 +1,63 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const MAX_PLAYS = 3;
+
+async function getPlays(trackId: number): Promise<number | undefined> {
+  try {
+    const id = String(trackId);
+    const plays = await AsyncStorage.getItem(id);
+
+    if (plays !== null) {
+      return Number(plays);
+    }
+
+    return 0;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function reachedMaxPlays(trackId: number) {
+  try {
+    const plays: number | undefined = await getPlays(trackId);
+
+    if (plays === undefined) {
+      console.error(
+        "Issue getting number of plays: 'number of plays undefined'"
+      );
+      return;
+    }
+
+    if (plays >= MAX_PLAYS) {
+      return true;
+    }
+
+    return false;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export async function incrementPlayCount(trackId: number) {
+  try {
+    const numPlays = await getPlays(trackId);
+
+    if (numPlays === undefined) {
+      console.error(
+        "Issue getting number of plays: 'number of plays undefined'"
+      );
+      return;
+    }
+
+    if (numPlays === 0) {
+      await AsyncStorage.setItem(String(trackId), "1");
+      return;
+    } else {
+      const updatedPlays = numPlays + 1;
+      await AsyncStorage.setItem(String(trackId), String(updatedPlays));
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
