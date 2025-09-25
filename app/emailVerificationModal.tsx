03128cea5aca133ae7cfa32c04fae4c8d438297c
rann143 <dan.roder17@gmail.com>
2025-09-25 15:56:54 -0400

    implement email verification flow for wishlist button if not logged in. Styling needed

--- app/emailVerificationModal.tsx
+++ app/emailVerificationModal.tsx
@@ -2,9 +2,11 @@ import { useAuthContext } from "@/state/AuthContext";
 import * as api from "../queries/fetch/fetchWrapper";
 import { useState, useCallback } from "react";
 import { useForm, Controller } from "react-hook-form";
-import { V } from "@faker-js/faker/dist/airline-BUL6NtOJ";
 import { useQueryClient } from "@tanstack/react-query";
-import { Pressable, TextInput, View, Text } from "react-native";
+import { Pressable, TextInput, View, Text, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { mirloRed } from "@/constants/mirlo-red";
+import { useRouter } from "expo-router";
 
 type VerifyEmailInputs = {
   email: string;
@@ -26,6 +28,7 @@ export default function emailVerificationModal() {
   const {
     control: controlCode,
     handleSubmit: handleSubmitCode,
+    reset: resetCode,
     formState: { errors: codeErrors },
   } = useForm<verifyCodeInputs>({
     defaultValues: {
@@ -38,6 +41,7 @@ export default function emailVerificationModal() {
   const [email, setEmail] = useState("");
   const [emailVerified, setEmailVerified] = useState(false);
   const queryClient = useQueryClient();
+  const router = useRouter();
 
   const verifyEmail = useCallback(
     async (emailInput: VerifyEmailInputs) => {
@@ -46,13 +50,15 @@ export default function emailVerificationModal() {
         await api.post("/auth/verify-email", emailInput);
         setEmail(emailInput.email);
         setWaitingForVerification(true);
+
+        resetCode();
       } catch (err) {
         console.error("issue verifying email", err);
       } finally {
         setIsLoading(false);
       }
     },
-    [email]
+    [email, resetCode]
   );
 
   const verifyCode = useCallback(
@@ -70,6 +76,7 @@ export default function emailVerificationModal() {
             queryClient.invalidateQueries({
               predicate: (query) => query.queryKey.includes("auth"),
             });
+            router.dismiss();
           }
         }
       } catch (err) {
@@ -81,33 +88,92 @@ export default function emailVerificationModal() {
     [email, queryClient, setEmailVerified]
   );
 
-  return waitingForVerification ? (
-    <View>
+  return !waitingForVerification ? (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.formLabel}>Email</Text>
       <Controller
-        control={controlCode}
-        rules={{ required: true }}
-        render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput onBlur={onBlur} onChangeText={onChange} value={value} />
-        )}
-        name="code"
-      />
-      <Pressable onPress={handleSubmitCode(verifyCode)}>
-        <Text>Verify</Text>
-      </Pressable>
-    </View>
-  ) : (
-    <View>
-      <Controller
+        key="email-controller"
         control={controlEmail}
         rules={{ required: true }}
         render={({ field: { onChange, onBlur, value } }) => (
-          <TextInput onBlur={onBlur} onChangeText={onChange} value={value} />
+          <TextInput
+            key="email-input"
+            style={styles.inputStyle}
+            placeholderTextColor="#555"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
         )}
         name="email"
       />
-      <Pressable onPress={handleSubmitEmail(verifyEmail)}>
-        <Text>Send Verification Code</Text>
+      <Pressable style={styles.button} onPress={handleSubmitEmail(verifyEmail)}>
+        <Text style={styles.text}>Verify</Text>
       </Pressable>
-    </View>
+    </SafeAreaView>
+  ) : (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.formLabel}>Verification Code</Text>
+      <Controller
+        key="code-controller"
+        control={controlCode}
+        rules={{ required: true }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            key="verification-code-input"
+            style={styles.inputStyle}
+            placeholderTextColor="#555"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="code"
+      />
+      <Pressable style={styles.button} onPress={handleSubmitCode(verifyCode)}>
+        <Text style={styles.text}>Send Verification Code</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: mirloRed,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    height: 50,
+    flex: 1,
+  },
+
+  formLabel: {
+    marginTop: 20,
+    fontSize: 20,
+    color: "#fff",
+  },
+  inputStyle: {
+    marginTop: 5,
+    width: 300,
+    height: 40,
+    paddingHorizontal: 10,
+    backgroundColor: "white",
+  },
+  formText: {
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#aaa",
+    fontSize: 20,
+  },
+  text: {
+    color: "#000",
+    fontSize: 20,
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FFF",
+    paddingVertical: 5,
+    borderRadius: 50,
+  },
+});
